f2cb2fb6f100247fd5c099c34e0fd4ff6278ad04
quasarwei <quasar.wei@outlook.com>
2020-05-16 18:33:01 -0700

    detect links from tweet bodies and insert link tags around them

--- src/components/search.js
+++ src/components/search.js
@@ -6,6 +6,7 @@ import stocktwitService from '../services/stocktwit-service';
 export default function Search() {
   const [term, setTerm] = useState(0);
   const dispatch = useDispatch();
+  const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
 
   const handleAddTerm = async e => {
     e.preventDefault();
@@ -17,12 +18,20 @@ export default function Search() {
     try {
       const symbolData = await stocktwitService.getSymbol(term);
       console.log(symbolData);
-
-      symbolData.messages.forEach(message => {
-        dispatch(addTweet(message));
-      });
+      if (symbolData) {
+        symbolData.messages.forEach(message => {
+          let linkedBody = message.body.replace(
+            urlRegex,
+            url =>
+              `<a href="${url}" target="_blank" rel="noopener noreferrer">${url}</a>`
+          );
+          let newMessage = { ...message, body: linkedBody };
+          dispatch(addTweet(newMessage));
+        });
+      }
     } catch (e) {
-      console.log(e.errors[0].message);
+      // console.log(e.errors[0].message);
+      console.log(e);
     }
   };
 
--- src/components/tweetcard.js
+++ src/components/tweetcard.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeTerm } from '../actions';
+import './tweetcard.css';
 
 export default function TweetCard(props) {
+  const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+
+  const makeBody = () => ({
+    __html: props.tweet.body
+  });
+
   return (
-    <div>
-      <h3>{props.tweet.body}</h3>
-      <h4>{props.tweet.user.username}</h4>
+    <div className="tweet">
+      {/* this method puts users at risk to xss attacks, try and find another way */}
+      <p className="tweet__body" dangerouslySetInnerHTML={makeBody()}></p>
+      <h4 className="tweet__user">{props.tweet.user.username}</h4>
     </div>
   );
 }
