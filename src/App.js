bb9092088e565cf30a8ee0b82fe3263c9d85fd86
quasarwei <quasar.wei@outlook.com>
2020-05-20 18:30:26 -0700

    put api endpoint in .env

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import './App.css';
 
 function App(props) {
   useEffect(() => {}, [props.symbols]);
+  console.log(process.env);
 
   // remove searchterm and corresponding tweet results from store
   const handleRemoveTerm = symbol => {
--- src/services/stocktwit-service.js
+++ src/services/stocktwit-service.js
@@ -1,8 +1,9 @@
+import config from '../config';
+
 const stocktwitService = {
   getSymbol(symbol, lasttweetID) {
     return fetch(
-      // `https://api.stocktwits.com/api/2/streams/symbol/${symbol}.json?since=${lasttweetID}`)
-      `https://orbis-stocktwits-search.herokuapp.com/api/2/streams/symbol/${symbol}.json?since=${lasttweetID}`
+      `${config.API_ENDPOINT}/api/2/streams/symbol/${symbol}.json?since=${lasttweetID}`
     ).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
